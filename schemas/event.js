6b57856a4cc05ccad2be052fba5706abad785dd5
github username <toniaroganti@gmail.com>
2022-06-09 16:10:56 -0400

    slug added

--- schemas/event.js
+++ schemas/event.js
@@ -29,6 +29,16 @@ export default {
 			type: 'string',
 			title: 'Time',
 		},
+		{
+			name: 'slug',
+			type: 'slug',
+			title: 'Slug',
+			options: {
+				source: 'title',
+				slugify: (input) =>
+					input.toLowerCase().replace(/\s+/g, '-').slice(0, 200),
+			},
+		},
 		{ name: 'image', title: 'Image', type: 'image' },
 	],
 };
